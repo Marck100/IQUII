bf6632d55c815990bc16b388b718581a76d23051
Marco Pilloni <marcopilloni@icloud.com>
2021-08-11 19:33:05 +0200

    + Post + RedditController
    Fetch posts by keywords

---
+++ IQUII/Controllers/RedditController.swift
@@ -0,0 +1,64 @@
+//
+//  RedditController.swift
+//  IQUII
+//
+//  Created by Marco Pilloni on 11/08/21.
+//  Copyright © 2021 Marco Pilloni. All rights reserved.
+//
+
+import Foundation
+
+
+final class RedditController {
+    
+    struct Result: Codable {
+        
+        var posts: [Post]
+        var url: URL!
+        
+        enum ListingCodingKeys: CodingKey {
+            case data
+        }
+        enum DataCodingKeys: CodingKey {
+            case children
+        }
+        
+        init(from decoder: Decoder) throws {
+            let listingContainer = try decoder.container(keyedBy: ListingCodingKeys.self)
+            let dataContainer = try listingContainer.nestedContainer(keyedBy: DataCodingKeys.self, forKey: .data)
+            
+            let posts = try dataContainer.decode([Post].self, forKey: .children)
+            self.posts = posts
+        }
+        
+        
+    }
+    
+    private let basePath = "https://www.reddit.com/r/{KEYWORD}/top.json"
+    
+    static let `default` = RedditController()
+    
+    func fetchPosts(_ keyword: String, completionHandler: @escaping(([Post]?) -> Void)) {
+        
+        let path = basePath.replacingOccurrences(of: "{KEYWORD}", with: keyword)
+        guard let url = URL(string: path) else {
+            completionHandler(nil)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            var posts: [Post]? = nil
+            defer { completionHandler(posts) }
+            guard let data = data else { return }
+            
+            guard let result = try? JSONDecoder().decode(Result.self, from: data) else { return }
+            posts = result.posts
+            
+        }.resume()
+        
+    }
+    
+}
---
+++ IQUII/Models/Post.swift
@@ -0,0 +1,58 @@
+//
+//  Post.swift
+//  IQUII
+//
+//  Created by Marco Pilloni on 11/08/21.
+//  Copyright © 2021 Marco Pilloni. All rights reserved.
+//
+
+import Foundation
+
+
+struct Post: Codable {
+    
+    enum DecodingError: Error {
+        case invalidPhotoURL
+    }
+    
+    var imageURL: URL!
+    var title: String
+    var description: String
+    
+    var subreddit: String
+    var author: String
+    
+    enum ChildrenCodingKeys: CodingKey {
+        case data
+    }
+    enum DataCodingKeys: CodingKey {
+        case subreddit, selftext, author_fullname, title, thumbnail
+    }
+    
+    init(from decoder: Decoder) throws {
+        let childrenContainer = try decoder.container(keyedBy: ChildrenCodingKeys.self)
+        
+        let container = try childrenContainer.nestedContainer(keyedBy: DataCodingKeys.self, forKey: .data)
+        let imagePath = try container.decode(String.self, forKey: .thumbnail)
+        guard let imageURL = URL(string: imagePath) else {
+            throw DecodingError.invalidPhotoURL
+        }
+        
+        self.imageURL = imageURL
+        title = try container.decode(String.self, forKey: .title)
+        description = try container.decode(String.self, forKey: .selftext)
+        subreddit = try container.decode(String.self, forKey: .subreddit)
+        author = try container.decode(String.self, forKey: .author_fullname)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: DataCodingKeys.self)
+        
+        try container.encode(imageURL.absoluteString, forKey: .thumbnail)
+        try container.encode(title, forKey: .title)
+        try container.encode(description, forKey: .selftext)
+        try container.encode(subreddit, forKey: .subreddit)
+        try container.encode(author, forKey: .author_fullname)
+    }
+    
+}
